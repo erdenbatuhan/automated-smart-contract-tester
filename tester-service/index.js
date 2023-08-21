2ca1fb330e36afa29149311a6a8c3f10ffd0ed16
Batuhan Erden <erden.batuhan@gmail.com>
2023-08-21 21:09:00 +0200

    add mongodb connection

--- tester-service/index.js
+++ tester-service/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const appProps = require("./application-properties.json");
 const app = express();
 
+// Connect to MongoDB
+mongoose
+  .connect(appProps["mongo"]["dbURI"], {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB!"))
+  .catch((err) => console.log(err));
+
 app.use(cors()); // CORS
 app.use(bodyParser.json({ limit: "50mb" })); // Parses the text as JSON and exposes the resulting object on req.body
 
+// Listen on the port specified
 app.listen(appProps["port"], () => {
   console.log(`Server is running on port ${appProps["port"]}`);
 });
