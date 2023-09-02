02b31a9d18fafd4536685f51ec680db8cea51196
Batuhan Erden <erden.batuhan@gmail.com>
2023-09-02 05:33:39 +0200

    remove Forge routes (Will be moved to the backend service)

--- app/test-runner/api-routes.js
+++ app/test-runner/api-routes.js
@@ -5,12 +5,10 @@ const router = express.Router();
 const routerUtils = require('./src/utils/router-utils');
 
 const healthCheckRoutes = require('./src/routes/health-check-routes');
-const forgeRoutes = require('./src/routes/forge-routes');
 const projectRoutes = require('./src/routes/project-routes');
 const executionRoutes = require('./src/routes/execution-routes');
 
 router.use('/', healthCheckRoutes);
-router.use('/forge', forgeRoutes);
 router.use('/project', projectRoutes);
 router.use('/project/:projectName/execution', (req, _, next) => {
   req.locals = { ...routerUtils.extractRequiredParams(req, ['projectName']) };
