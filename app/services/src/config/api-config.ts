d1ee195ec8f7e74446cf584c421968041ede0034
Batuhan Erden <erden.batuhan@gmail.com>
2023-09-09 19:41:23 +0200

    services: update api-config

--- app/services/src/config/api-config.ts
+++ app/services/src/config/api-config.ts
@@ -2,8 +2,8 @@
 const { APP_NAME, TESTRUNNER_SERVICE_NAME, TESTRUNNER_HOST } = process.env;
 if (!APP_NAME || !TESTRUNNER_SERVICE_NAME || !TESTRUNNER_HOST) throw new Error('Missing environment variables!');
 
-const TESTRUNNER_BASE_URL = `http://${TESTRUNNER_HOST}/api/${APP_NAME}/${TESTRUNNER_SERVICE_NAME}/v1`;
-const TESTRUNNER_URL_PROJECTS = `${TESTRUNNER_BASE_URL}/projects`;
+const TESTRUNNER_BASE = `http://${TESTRUNNER_HOST}/api/${APP_NAME}/${TESTRUNNER_SERVICE_NAME}/v1`;
+const TESTRUNNER_URL_PROJECTS = `${TESTRUNNER_BASE}/projects`;
 
 export default {
   TESTRUNNER_URL_PROJECT_UPLOAD: (projectName: string) => `${TESTRUNNER_URL_PROJECTS}/${projectName}/upload`
