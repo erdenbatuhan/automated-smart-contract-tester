b6818a501174b192bc32603f49e82363fe4a46dc
Batuhan Erden <erden.batuhan@gmail.com>
2023-09-09 09:31:38 +0200

    test-runner: run the tests with exec args, if provided

--- app/test-runner/src/routes/health-check-routes.ts
+++ app/test-runner/src/routes/health-check-routes.ts
@@ -16,7 +16,7 @@ router.post('/', async (req: Request, res: Response) => {
   });
 
   healthCheckPromise.then((result) => {
-    res.status(200).json(req.body);
+    res.status(200).send(result);
   }).catch((err: Error | unknown) => {
     res.status(500).send((err as Error)?.message);
   });
