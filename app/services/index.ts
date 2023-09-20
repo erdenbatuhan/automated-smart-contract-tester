c63df31ec86760b4eb4018f2699f35db3dfed184
Batuhan Erden <erden.batuhan@gmail.com>
2023-09-20 04:35:51 +0200

    service: update folder structure of the rest folder

--- app/services/index.ts
+++ app/services/index.ts
@@ -8,7 +8,9 @@ import mongoose from 'mongoose';
 import Logger from '@Logger';
 import AppError from '@errors/AppError';
 
-import apiRoutes from '@rest/apiRoutes';
+import projectMiddlewares from '@middlewares/projectMiddlewares';
+
+import apiRouter from '@rest';
 
 // Read environment variables
 const { APP_NAME, SERVICE_NAME, PORT, MONGODB_URI } = process.env;
@@ -20,17 +22,17 @@ app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
 app.use(helmet()); // Enhance security using Helmet middleware
 app.use(bodyParser.json({ limit: '50mb' })); // Parse JSON requests and set body size limit
 app.use(cookieParser()); // Enable cookie parsing
-app.use(`/api/${APP_NAME}/${SERVICE_NAME}/v1`, apiRoutes); // Mount modular routes with the common prefix
+app.use(`/api/${APP_NAME}/${SERVICE_NAME}/v1`, apiRouter); // Mount modular routes with the common prefix
 
 Promise.all([
   // (1) Establish a connection to MongoDB
   mongoose.connect(MONGODB_URI).catch((err: Error | unknown) => {
     throw AppError.createAppError(err, 'Could not connect to the DB.');
+  }),
+  // (2) Prepare the test runner service by uploading all projects to it
+  projectMiddlewares.prepareTestRunnerService().catch((err: Error | unknown) => {
+    throw AppError.createAppError(err, 'Could not prepare the test runner service.');
   })
-  // // (2) Prepare the test runner service by uploading all projects to it
-  // projectMiddlewares.prepareTestRunnerService().catch((err: Error | unknown) => {
-  //   throw AppError.createAppError(err, 'Could not prepare the test runner service.');
-  // })
 ]).then(() => {
   // (3) Start the application server on the specified port
   app.listen(PORT, () => {
