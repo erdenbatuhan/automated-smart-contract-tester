57cab249102c33814eb5c0fcce0e870998603614
Batuhan Erden <erden.batuhan@gmail.com>
2023-09-20 04:32:39 +0200

    test-runner: update folder structure of rest & rabbitmq folders

--- app/test-runner/index.ts
+++ app/test-runner/index.ts
@@ -7,8 +7,8 @@ import mongoose from 'mongoose';
 import Logger from '@Logger';
 import AppError from '@errors/AppError';
 
-import restRoutes from '@rest/restRoutes';
-import messageConsumers from '@rabbitmq/messageConsumers';
+import restRouter from '@rest';
+import rabbitmq from '@rabbitmq';
 
 // Read environment variables
 const { APP_NAME, SERVICE_NAME, PORT, MONGODB_URI } = process.env;
@@ -19,15 +19,15 @@ const app = express();
 app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
 app.use(helmet()); // Enhance security using Helmet middleware
 app.use(bodyParser.json({ limit: '50mb' })); // Parse JSON requests and set body size limit
-app.use(`/api/${APP_NAME}/${SERVICE_NAME}/v1`, restRoutes); // Mount modular routes with the common prefix
+app.use(`/api/${APP_NAME}/${SERVICE_NAME}/v1`, restRouter); // Mount modular routes with the common prefix
 
 Promise.all([
   // (1) Establish a connection to MongoDB
   mongoose.connect(MONGODB_URI).catch((err: Error | unknown) => {
     throw AppError.createAppError(err, 'Could not connect to the DB.');
   }),
-  // (2) Initialize the message consumers
-  messageConsumers.initializeMessageConsumers()
+  // (2) Initialize the RabbitMQ message consumers
+  rabbitmq.initializeMessageConsumers()
 ]).then(() => {
   // (3) Start the application server on the specified port
   app.listen(PORT, () => {
