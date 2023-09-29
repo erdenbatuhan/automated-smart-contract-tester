4befec3ef30d41affc6fb2886be703f4411be35c
Batuhan Erden <erden.batuhan@gmail.com>
2023-09-29 06:01:26 +0200

    services: add pre-save middleware for Project Config
    This commit introduces a pre-save middleware function for Project Configuration documents. This middleware ensures that certain fields have default values assigned before saving the document. Specifically, it initializes the 'tests' array, sets the 'containerTimeout' to a default value from Constants if not provided, and initializes 'testExecutionArguments' as an empty object if not provided. This ensures that the data is in the correct state before persisting it in the database.

--- services/src/models/schemas/ProjectConfigSchema.ts
+++ services/src/models/schemas/ProjectConfigSchema.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import type { CallbackWithoutResultAndOptionalError } from 'mongoose';
 
 import Constants from '@Constants';
 
@@ -9,15 +10,16 @@ import TestExecutionArgumentsSchema from '@models/schemas/TestExecutionArguments
 import type { ITestExecutionArguments } from '@models/schemas/TestExecutionArgumentsSchema';
 
 export interface IProjectConfig {
-  tests: ITest[];
+  tests?: ITest[];
   containerTimeout?: number;
   testExecutionArguments?: ITestExecutionArguments;
+
+  preSave(this: IProjectConfig, next: CallbackWithoutResultAndOptionalError): Promise<void>;
 }
 
-// ProjectConfigSchema
-export default new mongoose.Schema<IProjectConfig>(
+const ProjectConfigSchema = new mongoose.Schema<IProjectConfig>(
   {
-    tests: { type: [TestSchema], required: true },
+    tests: { type: [TestSchema] },
     containerTimeout: { type: Number, default: Constants.CONTAINER_TIMEOUT_DEFAULT },
     testExecutionArguments: { type: TestExecutionArgumentsSchema }
   },
@@ -25,3 +27,21 @@ export default new mongoose.Schema<IProjectConfig>(
     _id: false
   }
 );
+
+ProjectConfigSchema.pre<IProjectConfig>('save',
+  /**
+   * Middleware function to execute before saving a Project Configuration document.
+   *
+   * @param {CallbackWithoutResultAndOptionalError} next - The callback function to invoke after pre-processing.
+   * @returns {Promise<void>} A Promise that resolves when the pre-processing is complete.
+   */
+  async function preSave(this: IProjectConfig, next: CallbackWithoutResultAndOptionalError): Promise<void> {
+    this.tests = this.tests || [];
+    this.containerTimeout = this.containerTimeout || Constants.CONTAINER_TIMEOUT_DEFAULT;
+    this.testExecutionArguments = this.testExecutionArguments || {};
+
+    next();
+  }
+);
+
+export default ProjectConfigSchema;
